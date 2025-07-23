2a0c9137c484f694adc2d64af163b5631bf88dd8
jinyawen <jin2020190@gmail.com>
2025-07-23 22:38:45 +0800

    feat(admin): 添加用户管理功能- 新增用户管理相关的实体类、DTO、Mapper、Service和Controller - 实现用户信息查询功能 - 添加全局异常处理和统一返回结果封装 - 定义基础错误码和用户相关错误码

---
+++ admin/src/main/java/com/jinyawen/shortlink/admin/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.jinyawen.shortlink.admin.controller;
+
+import com.jinyawen.shortlink.admin.common.convention.result.Result;
+import com.jinyawen.shortlink.admin.common.convention.result.Results;
+import com.jinyawen.shortlink.admin.common.enums.UserErrorCodeEnum;
+import com.jinyawen.shortlink.admin.dto.resp.UserRespDTO;
+import com.jinyawen.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户管理控制层
+ */
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    /**
+     * 根据用户名查询用户信息
+     */
+    @GetMapping("/api/shortlink/v1/user/{username}")
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username){
+        UserRespDTO result = userService.getUserByUsername(username);
+
+        return Results.success(result);
+
+    }
+}
---
+++ admin/src/main/java/com/jinyawen/shortlink/admin/dao/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.jinyawen.shortlink.admin.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jinyawen.shortlink.admin.dao.entity.UserDO;
+
+/**
+ * 用户持久层
+ */
+public interface UserMapper extends BaseMapper<UserDO> {
+}

8a31edc09425bfef45a795c69967ffcc341d55f7
jinyawen <jin2020190@gmail.com>
2025-07-28 16:32:21 +0800

    fix(admin): 修复用户注册并发问题
    - 新增 RedisCacheConstant 类用于定义缓存常量- 在 UserServiceImpl 中添加分布式锁，解决并发注册时的穿透问题
    - 优化用户注册逻辑，确保数据一致性

---
+++ admin/src/main/java/com/jinyawen/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,8 @@
+package com.jinyawen.shortlink.admin.common.constant;
+
+/**
+ * redis缓存常量类
+ */
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+}
--- admin/src/main/java/com/jinyawen/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/jinyawen/shortlink/admin/service/impl/UserServiceImpl.java
@@ -2,7 +2,6 @@ package com.jinyawen.shortlink.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.jinyawen.shortlink.admin.common.convention.exception.ClientException;
@@ -14,8 +13,12 @@ import com.jinyawen.shortlink.admin.dto.resp.UserRespDTO;
 import com.jinyawen.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import static com.jinyawen.shortlink.admin.common.constant.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
 
 /**
  * 用户接口实现层
@@ -25,6 +28,7 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
 
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+    private final RedissonClient redissonClient;
     @Override
     public UserRespDTO getUserByUsername(String username) {
 
@@ -54,10 +58,20 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         if(!hasUsername(requestParam.getUsername())){
             throw new ClientException(UserErrorCodeEnum.USER_NAME_EXIST);
         }
-        int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
-        if(inserted<1){
-            throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+        RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY+requestParam.getUsername());
+        try {
+            if(lock.tryLock()){
+                int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+                if(inserted<1){
+                    throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+                }
+                userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+                return;
+            }
+            throw new ClientException(UserErrorCodeEnum.USER_NAME_EXIST);
+        }finally {
+            lock.unlock();
         }
-        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+
     }
 }
